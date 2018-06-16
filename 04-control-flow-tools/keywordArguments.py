f79e0d7a269ba44e1c5458cd9557ea453838e4a4
mattNat <mattsanpedro76@gmail.com>
2018-06-15 20:40:56 -0700

    add readme.txt for installing on other machines

--- 04-control-flow-tools/keywordArguments.py
+++ 04-control-flow-tools/keywordArguments.py
@@ -1,7 +1,33 @@
 def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
-  print("-- This parrot wouldn't", action, end=' ')
-  print("if you put", voltage, "volts through it.")
-  print("-- Lovely plummage, the", type)
-  print("-- It's", state, "!")
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plummage, the", type)
+    print("-- It's", state, "!")
+
 
 parrot(1000)
+parrot(voltage=1000)
+parrot(action='VOOOOM', voltage=100000)
+parrot('a million', 'life', 'jump')
+
+# parrot()                     # required argument missing
+# parrot(voltage=5.0, dead')  # non-keyword argument after a keyword argument
+# parrot(110, voltage=220)     # duplicate value for the same argument
+# parrot(actor='John Cleese')  # unknown keyword argument
+
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
